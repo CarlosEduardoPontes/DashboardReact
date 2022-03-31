583b56f44b1ade804699fdba3d32e049b20f52d3
Carlos Eduardo <ceduardo.pontes@hotmail.com>
2022-03-31 08:13:36 -0300

    Atualizaçoes

--- dashboard/src/pages/Home/Aluno/Aluno.js
+++ dashboard/src/pages/Home/Aluno/Aluno.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../Home.css';
-
+import { Container, Row, Col } from 'react-grid-system';
 import { Switch, Route } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
 import StoreContext from '../../../components/store/Context';
@@ -13,7 +13,30 @@ function HomeAluno(){
 
   return (
     <Router>
-      <SideBar tipo='aluno' />  
+      <SideBar tipo='aluno' /> 
+
+            <Container className='table'>
+              <Row>
+                <Col xs={4} className='coluna'>
+                  <h3>História</h3>
+                  <p>Conteudo do campo historia</p>
+                  <p>Conteudo do campo historia</p>
+                  <p>Conteudo do campo historia</p>
+                </Col>
+                <Col sm={4} className='coluna'>
+                  <h3>Geografia</h3>
+                  <p>Conteudo do campo Geografia</p>
+                  <p>Conteudo do campo Geografia</p>
+                  <p>Conteudo do campo Geografia</p>
+                </Col>
+                <Col sm={4} className='coluna'>
+                  <h3>Portugues</h3>
+                  <p>Conteudo do campo Portugues</p>
+                  <p>Conteudo do campo Portugues</p>
+                  <p>Conteudo do campo Portugues</p>
+                </Col>
+              </Row>
+            </Container> 
       <Switch> 
         <Route path='/home/aluno ' exact component={OverviewAluno} />
         <Route path='overview' exact component={OverviewAluno} />      
