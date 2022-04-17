01bf0bff303ed2876f898b9af3b05e6576673be9
Carlos Eduardo <ceduardo.pontes@hotmail.com>
2022-04-17 15:06:56 -0300

    Cadastro de exercícios

--- dashboard/src/pages/Cadastro/Professor/Exercicio.js
+++ dashboard/src/pages/Cadastro/Professor/Exercicio.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect, useReducer } from 'react';
 import Select from 'react-select'
 import axios from 'axios'
 import StoreContext from '../../../components/store/Context';
@@ -18,6 +18,7 @@ const initialState = () => {
 }
 
 export const Exercicio = props =>{
+    const [, forceUpdate] = useReducer(x => x + 1, 0);
     const { session } = useContext(StoreContext);
 
     const [spanErro, setSpanErro] = useState('');
@@ -27,20 +28,45 @@ export const Exercicio = props =>{
     const [name, setName] = useState('');
     const [id, setId] = useState(0);
 
+    const [materia, setMateria] = useState([]);
+
+    const [alternativa, setAlternativa] = useState([]);
+
     const handleChange = (event) => {
         
         const { value, name } = event.target;
-    
+
         setForm({
           ...form,
           [name]: value
         });
+        console.log(form);
+    }
+
+    const handleCategoria = (e) => {
+
+        setForm(
+            {
+                ...form,
+                categoria: e.target.value
+            }
+        );
+    }
+
+    const handleMateria = (e) => {
+
+        setForm(
+            {
+                ...form,
+                idMateria: e.target.value
+            }
+        );
     }
 
     const onSubmit = (ev) => {
         ev.preventDefault();
 
-        form.idInstituicao = session.id;
+        form.idProfessor = session.id;
         console.log(form);
 
        
@@ -73,18 +99,107 @@ export const Exercicio = props =>{
         getOptions()
       }
 
+      const getMateria = async () => {
+        const materias = await api('/list/professor/materia/' + session.id, 'POST', form);
+
+        setMateria(materias);
+      };
+
       useEffect( () => {
-        componentDidMount();
+        //componentDidMount();
+        getMateria();
       }, []);
+
+      const addAlternativa = () =>{
+        const novaAlternativa = {
+            numerador: alternativa.length + 1,
+            resposta: ''
+        }
+
+        const alternativas = alternativa;
+        alternativas.push(novaAlternativa);
+
+        setAlternativa(alternativas);
+
+        console.log(alternativa);
+        forceUpdate();
+      };
+
+      const removeAlternativa = () =>{
+        const alternativas = alternativa;
+        alternativas.pop();
+
+        setAlternativa(alternativas);
+
+        console.log(alternativa);
+        forceUpdate();
+      };
+
+      const handleAlternativa = (event) => {
+        const { value, name } = event.target;
+
+        const alternativas = alternativa;
+        alternativas[name - 1] = {
+            numerador:alternativas[name-1]['numerador'],
+            resposta: value
+        }
+
+        setAlternativa(alternativas);
+
+        console.log(alternativa);
+      }
+
+      function renderiza(){
+          
+        if(form.categoria === 'ALTERNATIVA'){
+
+          return(
+            <>
+                <div>
+                    <button type='button' className='btn-add-alternativa' onClick={addAlternativa}>Adicionar Alternativas</button>
+                    <button type='button' className='btn-remove-alternativa' onClick={removeAlternativa}>Remover Alternativas</button>
+                </div><br></br>
+
+                <div>
+                    {alternativa.map( a => {
+
+                        const textarea = (
+                            <textarea 
+                            type='text' 
+                            placeholder={'Alternativa '+a.numerador}
+                            onChange={handleAlternativa}
+                            defaultValue={a.resposta}
+                            name={a.numerador}
+                        ></textarea>
+                        );
+
+                        return textarea
+                    }
+                        
+                    )}
+                </div>
+            </>
+                );
+        }
+        else{
+            return null;
+        }  
+
+      }
 
     return(
         <main class='container'>
             <form class='card' onSubmit={onSubmit}>
                 <h3>Cadastro de Exercício</h3>
                 
-                
-               
-                
+                <div class='label-float'>
+                    <select onChange={handleMateria} defaultValue={form.idMateria}>
+                        {materia.map( m => 
+                            <option >teste</option>
+                            )}
+                    </select>
+                </div>
+
                 <div class='label-float'>
                     <input 
                         type='text' 
@@ -107,14 +222,17 @@ export const Exercicio = props =>{
                 </div>
                 
                 <div class='label-float'>
-                    <select onChange={handleChange} defaultValue={form.categoria}>
+                    <select onChange={handleCategoria} defaultValue={form.categoria}>
                         <option value="">Selecione a categoria</option>
                         <option value="ALTERNATIVA">Alternativa</option>
                         <option value="DISSERTATIVA">Dissertativa</option>
                         <option value="CALCULO">Cálculo</option>
                     </select>
                 </div>
-
+                {
+                    renderiza()
+                }
+                
                 <span className='span-erro'>{spanErro}</span>
                 
                 <div>
