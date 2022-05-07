85553cd6edac0eee8a76f521f8db348af72bda93
viktorlnz <lnz.viktor0@gmail.com>
2022-05-07 15:13:42 -0300

    lista tarefas

--- dashboard/src/pages/Cadastro/Aluno/Tarefa.js
+++ dashboard/src/pages/Cadastro/Aluno/Tarefa.js
@@ -1,14 +1,35 @@
-import react from 'react';
+import react, { useContext, useEffect, useState } from 'react';
+import StoreContext from '../../../components/store/Context';
 import api from '../../../utils/api';
 
 export const Tarefa = props =>{
+
+    const [tarefas, setTarefas] = useState([]);
+    const { session } = useContext(StoreContext);
+    useEffect(() => {
+        const getTarefas = async () => {
+            const tarefas = await api('/tarefa/aluno/'+session.id+'/get_atuais');
+
+            console.log(tarefas);
+
+            setTarefas(tarefas);
+        };
+
+        getTarefas();
+    }, []);
     return(
         <main class='container'>
             <h1>Tarefas dos Alunos</h1>
-            <h2>Matéria:</h2>
-            <h2>Título da tarefa:</h2>
-            <h2>Pontos:</h2>
-            <h2>Termina em: {  }</h2>
+            {tarefas.map( t => (
+            <div className='div-tarefa'>    
+                <h2>{t.materia}</h2>
+                <h2>{t.titulo}</h2>
+                <h2>Pontos: {t.pontos}</h2>
+                <h2>Termina em: { t.dt_fim }</h2>
+            </div>
+            )
+            )}
+            
         </main>
     )
 }
